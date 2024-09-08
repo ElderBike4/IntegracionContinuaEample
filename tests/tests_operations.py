02026cce0f06d543735ac4fc4eef7a7a7eccc4e5
ElderBike4 <estebanelder4@gmail.com>
2024-09-08 17:38:55 -0600

    Pagina web sin operaciones

--- tests/tests_operations.py
+++ tests/tests_operations.py
@@ -1,38 +1,63 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Configuración del navegador
-driver = webdriver.Chrome()
+class TestOperations(unittest.TestCase):
 
-# Cargar la página web
-driver.get("http://localhost:8081")
+    @classmethod
+    def setUpClass(cls):
+        # Configuración del navegador
+        cls.driver = webdriver.Chrome()
+        cls.driver.get("http://localhost:8081")
+        cls.wait = WebDriverWait(cls.driver, 10)
 
-# Crear una instancia de WebDriverWait
-wait = WebDriverWait(driver, 10)
+    def test_suma(self):
+        # Esperar a que los campos num1 y num2 estén visibles
+        num1 = self.wait.until(EC.visibility_of_element_located((By.ID, 'num1')))
+        num2 = self.wait.until(EC.visibility_of_element_located((By.ID, 'num2')))
+        resultado = self.wait.until(EC.visibility_of_element_located((By.ID, 'resultado')))
 
-try:
-    # Esperar a que los campos num1 y num2 estén visibles
-    num1 = wait.until(EC.visibility_of_element_located((By.ID, 'num1')))
-    num2 = wait.until(EC.visibility_of_element_located((By.ID, 'num2')))
-    resultado = wait.until(EC.visibility_of_element_located((By.ID, 'resultado')))
+        # Ingresar valores
+        num1.send_keys("10")
+        num2.send_keys("5")
 
-    # Ingresar valores
-    num1.send_keys("10")
-    num2.send_keys("5")
+        # Probar suma
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Suma']"))).click()
+        self.wait.until(EC.text_to_be_present_in_element((By.ID, 'resultado'), "15"))
 
-    # Probar suma
-    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Suma']"))).click()
-    wait.until(EC.text_to_be_present_in_element((By.ID, 'resultado'), "15"))
+    def test_resta(self):
+        # Repetir la configuración para la resta
+        num1 = self.wait.until(EC.visibility_of_element_located((By.ID, 'num1')))
+        num2 = self.wait.until(EC.visibility_of_element_located((By.ID, 'num2')))
+        resultado = self.wait.until(EC.visibility_of_element_located((By.ID, 'resultado')))
 
-    # Probar resta
-    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Resta']"))).click()
-    wait.until(EC.text_to_be_present_in_element((By.ID, 'resultado'), "5"))
+        # Ingresar valores
+        num1.send_keys("10")
+        num2.send_keys("5")
 
-    # Probar multiplicación
-    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Multiplicar']"))).click()
-    wait.until(EC.text_to_be_present_in_element((By.ID, 'resultado'), "50"))
+        # Probar resta
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Resta']"))).click()
+        self.wait.until(EC.text_to_be_present_in_element((By.ID, 'resultado'), "5"))
 
-finally:
-    driver.quit()
+    def test_multiplicacion(self):
+        # Repetir la configuración para la multiplicación
+        num1 = self.wait.until(EC.visibility_of_element_located((By.ID, 'num1')))
+        num2 = self.wait.until(EC.visibility_of_element_located((By.ID, 'num2')))
+        resultado = self.wait.until(EC.visibility_of_element_located((By.ID, 'resultado')))
+
+        # Ingresar valores
+        num1.send_keys("10")
+        num2.send_keys("5")
+
+        # Probar multiplicación
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Multiplicar']"))).click()
+        self.wait.until(EC.text_to_be_present_in_element((By.ID, 'resultado'), "50"))
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
